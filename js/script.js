db4114beb56bd5c0c7639a5b418ae4423e267f61
RenatoFringuello <renatofringuello@hotmail.it>
2022-12-01 13:07:14 +0100

    add fn getRandomInclusive n generateBombPosition

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+/**
+ * 
+ * @param {*} tagName the tag to add in html
+ * @param {*} className the class of the tag; you can write a class list like in html
+ * @param {*} content the content of the tag to append into
+ * @returns the element 
+ */
 function createElement(tagName, className = '', content = ''){
     const element = document.createElement(tagName);
     element.className = className;
@@ -7,31 +14,72 @@ function createElement(tagName, className = '', content = ''){
 
 /**
  * 
- * @param {*} n number of cell to create, must be the pow of a number
- * @param {*} obj parent to create cells into
+ * @param {*} min min range is inclusive
+ * @param {*} max max range is inclusive
+ * @returns random number between min and max 
  */
-function createGame(n, obj){
-    for (let i = 0; i < n; i++) {
+function getRandomInclusive(min = 0, max){
+    return Math.floor(Math.random() * (max - min + 1) + min);;
+}
+
+/**
+ * 
+ * @param {*} nBombs number of bombs to generate
+ * @param {*} nCells number of cells to randomly plant nBombs into
+ */
+function generateBombsPosition(nBombs, collectionLength){
+    const bombPositons = [];
+    while(bombPositons.length < nBombs){
+        let randomIndex = getRandomInclusive(0, collectionLength);
+        if(!bombPositons.includes(randomIndex)){
+            bombPositons.push(randomIndex);
+        }
+    }
+    return bombPositons;
+}
+
+/**
+ * 
+ * @param {*} nCells number of cell to create, must be the pow of a number
+ * @param {*} obj parent to create gamefield into
+ */
+function createGame(nCells, obj){
+    obj.innerHTML = '';
+    const gameField = createElement('div', 'd-flex flex-wrap m-auto game-field');
+    obj.append(gameField);
+
+    const bombPositions = generateBombsPosition(16, nCells);
+
+    for (let i = 0; i < nCells; i++) {
+        let classReveal = 'blue';//if is a bomb the class is 'bomb' otherwise is 'blue'
         const cell = createElement('div', 'd-flex text-center cell');
         cell.append(createElement('span', 'm-auto', i + 1));
         
-        cell.addEventListener('click', function(){
-            cell.classList.toggle('blue');
-        });
+        if(bombPositions.includes(i)){
+            //there is a bomb at i position
+            classReveal = 'bomb';
+        }
 
-        obj.append(cell);
+        cell.addEventListener('click', function(){
+            cell.classList.add(classReveal);
+            if(classReveal === 'bomb'){
+                alert('game over');
+            }
+        },{once : true});
+
+        gameField.append(cell);
     }
 }
 
+
+
+//init
 const game = document.getElementById('game');
-const gameField = createElement('div', 'd-flex flex-wrap m-auto game-field');
 const playBtn = document.getElementById('play-btn');
 
+//play click
 playBtn.addEventListener('click', function(){
-    if(game.children.length === 0){
-        game.append(gameField);
-        createGame(100, gameField);
-    }
+    createGame(100, game);
 });
 
 
